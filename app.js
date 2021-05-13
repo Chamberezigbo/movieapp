2a3832f7975c644ec4da9b0b97846111062b9be6
chamberezigbo <chamberzigbo@gmail.com>
2021-05-13 23:30:57 +0100

    change port to 3000

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const request = require('request');
-const port = process.env.PORT || 3500;
+const port = process.env.PORT || 3000;
 
 
 app.set('views', path.join(__dirname,'views'))
