a841d40b9c935c1b5d76bcb82bc61da0a632ace4
chamberezigbo <chamberzigbo@gmail.com>
2021-06-08 12:31:35 +0100

    fixed an issue with app.js v-2

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.set('views', path.join(__dirname,'views'))
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+       res.render('index')
+})
+
 app.get('/result', (req,res) => {
        let query = req.query.search
        let url = `https://api.themoviedb.org/3/search/movie?api_key=167d21d032ef70cb07c0222951c15d10&query=${query}`;
@@ -21,7 +25,5 @@ app.get('/result', (req,res) => {
        })
        
 })
-app.get('/', (req, res) => {
-       res.render('index')
-})
+
 app.listen(port, () => console.log(`Server started at port ${port}`))
